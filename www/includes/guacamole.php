f0ef0a642814da986223fdaae225d56c92707c47
Jefferson Martin <jeff@jeffersonmartin.com>
2014-06-18 12:22:26 -0700

    Fixed non-user function to remove legacy user credential fields.

--- www/includes/guacamole.php
+++ www/includes/guacamole.php
@@ -30,8 +30,6 @@ function guacamole_url($base_url,$conn_id,$hostname,$protocol,$secret) {
 	$guac_conn_id		= $conn_id;
 	$guac_hostname 		= $hostname;
 	$guac_protocol		= $protocol;
-	$guac_username		= 'elabstudent';
-	$guac_password		= 'myeLab!';
 	$guac_secret		= $secret;
 
 	/* Set Default Value for Port Number */
@@ -46,13 +44,13 @@ function guacamole_url($base_url,$conn_id,$hostname,$protocol,$secret) {
 	$timestamp = time() * 1000;
 
 	/* Create Signature (Checksum) using Concatenated Variables */
-	$signature_concatenate = $timestamp.$guac_protocol.'hostname'.$guac_hostname.'port'.$guac_port.'username'.$guac_username.'password'.$guac_password;
+	$signature_concatenate = $timestamp.$guac_protocol.'hostname'.$guac_hostname.'port'.$guac_port;
 
 	/* Encode Signature (Checksum) using Secret (Salt) */
 	$signature_encode = base64_encode(hash_hmac('sha1', $signature_concatenate, $guac_secret, 1));
 
 	/* Create URL String with Concatenated Key Pair Variables */
-	$guacamole_url = $guac_base_url.'?id=c/'.$guac_conn_id.'&guac.protocol='.$guac_protocol.'&guac.hostname='.$guac_hostname.'&guac.port='.$guac_port.'&guac.username='.$guac_username.'&guac.password='.$guac_password.'&amp;timestamp='.$timestamp.'&signature='.$signature_encode;
+	$guacamole_url = $guac_base_url.'?id=c/'.$guac_conn_id.'&guac.protocol='.$guac_protocol.'&guac.hostname='.$guac_hostname.'&guac.port='.$guac_port.'&amp;timestamp='.$timestamp.'&signature='.$signature_encode;
 
 	/* Return URL String to User */
 	echo $guacamole_url;
